87856e4b8fb0f8b0ad4bfbaf5a10aa23992e60cf
pirating <42692867+pirating@users.noreply.github.com>
2020-08-09 00:47:56 -0700

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR BOT'S TOKEN HERE")                
+client.run("NzQxOTIyNjI5NzY1NTYyNDU5.Xy-nSw.4XZ1wleOX1TwFzlPyL5ywCViONo")                
